feba1e1944def98fa2f57a2867add98a0a88e0a2
Ramses Godoy <74273214+Ramsescce@users.noreply.github.com>
2021-01-18 16:11:25 -0300

    Listajs
    Desenvolvido na  primeira semana do Curso ABinbev

---
+++ lista/lista.js
@@ -0,0 +1,27 @@
+class Lista {
+  constructor() {
+    this.inputItem = document.querySelector("#item")
+    this.botaoAdicionar = document.querySelector("#adicionar")
+    this.lista = document.querySelector("#lista")
+    this.botaoApagar = document.querySelector("#apagar")
+  }
+
+  apagarLista(minhaLista) {
+    
+   localStorage.removeItem()
+   
+  }
+
+  adicionarNaLista(minhaLista) {
+    
+ //   let listaHTML = ''
+   //listaHTML.map(function(lista){
+
+   // this.lista.innerHTML = listaHTML
+   //})
+    // loop
+
+    
+  }
+
+} 
